ab43b7533c0ae6da9c08168f4d923be96201995e
yusufokkabas@windowslive.com <yusufokkabas@windowslive.com>
2023-10-30 08:56:11 +0300

    player controller fixes

--- src/controllers/player.controller.js
+++ src/controllers/player.controller.js
@@ -11,6 +11,15 @@ const getPlayerStatistics = async (req, res) => {
     throw new APIError("Error!", 400);
   }
 };
+function sumPropertiesOfObjects(arr, propertyNames) {
+  const sums = {};
+  
+  propertyNames.forEach(propertyName => {
+    sums[propertyName] = arr.reduce((accumulator, currentValue) => accumulator + currentValue[propertyName], 0);
+  });
+  
+  return sums;
+}
 
 const savePlayerStatistics = async (req, res) => {
   try {
@@ -20,84 +29,44 @@ const savePlayerStatistics = async (req, res) => {
     getPlayerStatistics.url = config.AXIOS_URL+"players";
     getPlayerStatistics.params=req.query;
     const playerStatistics = await axios(getPlayerStatistics);
-    console.log(JSON.stringify(playerStatistics.data.response))
     mappedData = await playerStatistics.data.response.map((item) => {
-      let data = {
-        general_player_statistic:{
-        id : item.player.id,
-        name: item.player.name,
-        first_name: item.player.firstname,
-        last_name: item.player.lastname,
-        age: item.player.age,
-        nationality:item.player.nationality,
-        height:item.player.height,
-        weight:item.player.weight,
-        injured:item.statistics.injured,
-        photo:item.player.photo
-      },
-      teams:{
-        id:item.statistics.team.id,
-        name:item.statistics.team.name,
-        logo:item.statistics.team.logo
-      },
-      game_info:{
-        appearances:item.statistics.games.appearences,
-        lineups:item.statistics.games.lineups,
-        minutes:item.statistics.games.minutes_played,
-        number:item.statistics.games.number,
-        position:item.statistics.games.position,
-        rating:item.statistics.games.rating,
-        captain:item.statistics.games.captain,
-        subbed_in:item.statistics.games.substitutes.in,
-        subbed_out:item.statistics.games.substitutes.out,
-        bench:item.statistics.games.substitutes.bench
-      },
-      shots:{
-        total:item.statistics.shots.total,
-        on:item.statistics.shots.on
-      },
-      goals:{
-        total:item.statistics.goals.total,
-        conceded:item.statistics.goals.conceded,
-        assists:item.statistics.goals.assists,
-        saves:item.statistics.goals.saves
-      },
-      passes:{
-        total:item.statistics.passes.total,
-        key:item.statistics.passes.key,
-        accuracy:item.statistics.passes.accuracy
-      },
-      tackles:{
-        total:item.statistics.tackles.total,
-        blocks:item.statistics.tackles.blocks,
-        interceptions:item.statistics.tackles.interceptions
-      },
-      duels:{
-        total:item.statistics.duels.total,
-        won:item.statistics.duels.won
-      },
-      dribbles:{
-        attempts:item.statistics.dribbles.attempts,
-        success:item.statistics.dribbles.success
-      },
-      fouls:{
-        drawn:item.statistics.fouls.drawn,
-        committed:item.statistics.fouls.committed
-      },
-      cards:{
-        yellow:item.statistics.cards.yellow,
-        yellowred:item.statistics.cards.yellowred,
-        red:item.statistics.cards.red
-      },
-      penalty:{
-        won:item.statistics.penalty.won,
-        commited:item.statistics.penalty.commited,
-        scored:item.statistics.penalty.scored,
-        missed:item.statistics.penalty.missed,
-        saved:item.statistics.penalty.saved
-      }
-      }
-      return data; 
+      let statisticsData = {
+        teams: {},
+        game_info: {},
+        games: {},
+        shots: {},
+        goals: {},
+        passes: {},
+        tackles: {},
+        duels: {},
+        dribbles: {},
+        fouls: {},
+        cards: {},
+        penalty: {}
+      };
+      statisticsData.teams.id = item.statistics[0].team.id;
+      statisticsData.teams.name = item.statistics[0].team.name;
+      statisticsData.teams.logo = item.statistics[0].team.logo;
+      statisticsData.game_info.position = item.statistics[0].games.position;
+      statisticsData.game_info.number = item.statistics[0].games.number;
+      statisticsData.game_info.captain = item.statistics[0].games.captain;
+      let ratingTotal=0;
+      item.statistics.forEach((element) => {
+       let rating = parseInt(element.games.rating);
+       ratingTotal=ratingTotal+rating;
+      });
+      let avgRating = ratingTotal/item.statistics.length;
+      statisticsData.game_info.rating = avgRating;
+      sumElements = ['games.appearences','games.lineups','games.minutes','substitutes.in','substitutes.out','substitutes.bench','shots.total','shots.on','goals.total','goals.conceded','goals.assists','goals.saves','passes.total','passes.key','passes.accuracy','tackles.total','tackles.blocks','tackles.interceptions','duels.total','duels.won','dribbles.attempts','dribbles.success','fouls.drawn','fouls.committed','cards.yellow','cards.yellowred','cards.red','penalty.won','penalty.commited','penalty.scored','penalty.missed','penalty.saved'];
+      sumElements = sumElements.map((item) => {
+        return item.split('.');
+      });
+      sumElements.forEach(element => {
+        statisticsData[element[0]][element[1]] = item.statistics.reduce((a, b, index) => {
+          return a + (b[element[0]][element[1]] || 0);
+        }, 0);
+      });
+      return statisticsData; 
     });
     console.log(mappedData);
   } catch (error) {
