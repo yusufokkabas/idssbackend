3adb95681ebeb1cee8c9bf770a43728e5a042e28
Yusuf Orhan Okkabas <yusufokkabas@gmail.com>
2023-10-31 17:57:28 +0300

    cors added

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const app = express();
 const port = process.env.PORT || 5001;
 const router = require("./src/routers");
 const errorHandlerMiddleware = require("./src/middlewares/errorHandler");
-
+const cors = require("cors");
+const c = require("config");
 // Middleware
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
-
+app.use(cors());
 app.use("/", router);
 
 app.get("/", (req, res) => {
